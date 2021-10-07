73b83ad547e6de859cdfb14ec5d0084762bf5c2b
Aslan <asl.zurabov@gmail.com>
2021-10-07 14:40:58 +0300

    class based Api views(get,post class and detail put and delete class)

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import article_list, article_detail
+from .views import article_list, article_detail,ArticleAPIView,ArticleDetails
 urlpatterns = [
 
-    path('article/',article_list,name='article'),
-    path('detail/<int:pk>',article_detail,name='detail'),
+    # path('article/',article_list,name='article'),
+    path('article/',ArticleAPIView.as_view(),name='article'),
+    # path('detail/<int:pk>',article_detail,name='detail'),
+    path('detail/<int:id>',ArticleDetails.as_view(),name='detail')
 ]
