376225d50024fae064917617bce03b522c318502
Aslan <asl.zurabov@gmail.com>
2021-10-08 12:46:43 +0300

    viewsets and routers

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,20 @@
-from django.urls import path
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import (article_list,
                     article_detail,
                     ArticleAPIView,
                     ArticleDetails,
                     GenericAPIView,
+                    ArticleViewSet
 )
 
-urlpatterns = [
+router = DefaultRouter()
+router.register('article',ArticleViewSet,basename='article')
 
+urlpatterns = [
+    path('viewset/',include(router.urls)),
+    path('viewset/<int:pk>/',include(router.urls)),
     # path('article/',article_list,name='article'),
     path('article/', ArticleAPIView.as_view(), name='article'),
     path('generic/article/<int:id>/', GenericAPIView.as_view(), name='article'),
@@ -16,4 +22,4 @@ urlpatterns = [
     path('detail/<int:id>', ArticleDetails.as_view(), name='detail')
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+# urlpatterns = format_suffix_patterns(urlpatterns)
